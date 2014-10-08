37bc4f5a763e9b45be95e0992278e0aa202d4d36
JD Schmidt <jd.schmidt@codefutures.com>
2014-10-08 11:32:29 -0600

    updating Java docs

---
+++ java/hard/LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+    public static void main ( String[] args )
+    {
+        System.out.println("+--------------------------------------+");
+        System.out.println("|                                  ### |");
+        System.out.println("|                                  ### |");
+        System.out.println("|                                  ### |");
+        System.out.println("|                                      |");
+        System.out.println("|                                      |");
+        System.out.println("|                   JD Schmidt         |");
+        System.out.println("|                   2 S. Ruby St.      |");
+        System.out.println("|                   Boise, ID          |");
+        System.out.println("|                                      |");
+        System.out.println("+--------------------------------------+");
+    }
+}
